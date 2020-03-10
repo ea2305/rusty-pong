897947af041bba372aa2f02618684782407222ba
Elihu Cruz <elihuacruz@gmail.com>
2020-03-09 20:20:06 -0600

    update example pong

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,10 @@ use amethyst::{
         RenderingBundle,
     },
     utils::application_root_dir,
+    input::{InputBundle, StringBindings}
 };
 
+mod systems;
 mod pong;
 use crate::pong::Pong;
 
@@ -20,21 +22,29 @@ fn main() -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
 
-    let game_data = GameDataBuilder::default().with_bundle(
-        RenderingBundle::<DefaultBackend>::new()
-            // The RenderToWindow plugin provides all the scaffolding for opening a window and
-            // drawing on it
-            .with_plugin(
-                RenderToWindow::from_config_path(display_config_path)
-                    .with_clear([0.0, 0.0, 0.0, 1.0]),
-            )
-            // RenderFlat2D plugin is used to render entities with `SpriteRender` component.
-            .with_plugin(RenderFlat2D::default()),
-    )?
-    .with_bundle(TransformBundle::new())?;
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle = InputBundle::<StringBindings>::new()
+        .with_bindings_from_file(binding_path)?;
+
+    let mut world = World::new();
+    let game_data = GameDataBuilder::default()
+        .with_bundle(input_bundle)?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                // The RenderToWindow plugin provides all the scaffolding for opening a window and
+                // drawing on it
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                // RenderFlat2D plugin is used to render entities with `SpriteRender` component.
+                .with_plugin(RenderFlat2D::default()),
+        )?
+        .with_bundle(TransformBundle::new())?;
 
     let assets_dir = app_root.join("assets");
-    let mut world = World::new();
     let mut game = Application::new(assets_dir, Pong, game_data)?;
 
     game.run();
--- src/pong.rs
+++ src/pong.rs
@@ -9,6 +9,7 @@ use amethyst::{
 };
 
 pub struct Pong;
+
 pub const ARENA_HEIGHT: f32 = 100.0;
 pub const ARENA_WIDTH: f32 = 100.0;
 pub const PADDLE_HEIGHT: f32 = 16.0;
